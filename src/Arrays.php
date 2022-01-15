78a7540aa762fbd29f62fbf73ea4618c4846d2d2
Murilo Perosa <perosamurilo@gmail.com>
2022-01-15 16:52:03 -0300

    Add Arrays.php.

---
+++ src/Arrays.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace MuriloPerosa\SuperPowers;
+
+class Arrays {
+
+    /**
+     * Remove duplicate elements of array recursively
+     *
+     * @param array $array
+     * @return array
+     */
+    public static function uniqueRecursive ($array)
+    {
+        $result = array_map("unserialize", array_unique(array_map("serialize", $array)));
+
+        foreach ($result as $key => $value)
+        {
+            if(is_array($value))
+            {
+                $result[$key] = self::uniqueRecursive($value);
+            }
+        }
+
+        return $result;
+    }
+
+
+    /**
+     * Removes all empty or null values from array
+     * @param  array $array
+     * @return array
+     */
+    public static function removeEmptyValues($array)
+    {
+        if (empty($array) || !is_array($array))
+        {
+            return [];
+        }
+        
+        $array = array_map(function($val){
+            return trim($val);
+        }, $array);
+
+        $array = array_filter($array);
+        return $array;
+    }
+} 
